07c98791f48b3cc8d1ef6312829b2a3e6f98c3d2
Yvo Brevoort <yvo@muze.nl>
2025-03-31 11:53:37 +0200

    update namespace

--- src/Ariadne.php
+++ src/Ariadne.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Pdsinterop\Flysystem\Adapter;
+namespace MuzeNl\Flysystem\Adapter;
 
 use League\Flysystem\AdapterInterface;
 use League\Flysystem\Config;
